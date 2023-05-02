63a1a6884f89b6a1e2b361e7046cc1b29425e6e7
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-05-03 02:22:41 +0800

    feat: 2x2 grid for options selection

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -81,67 +81,77 @@ const Home = () => {
             </div>
             <div className='mt-5'>
                 <hr/>
-                <h2 className='mt-3'>Eat Where? <span style={{ color: 'blue' }}>{selectedLocation}</span></h2>
-                <Dropdown size="lg">
-                    <Dropdown.Toggle 
-                        variant="primary" 
-                        id="dropdown-basic" 
-                    >
-                        Select a location
-                    </Dropdown.Toggle>
+                <div className='row'>
+                    <div className='col-6 pe-0'>
+                        <h2>Eat Where? <span style={{ color: 'blue' }}>{selectedLocation}</span></h2>
+                        <Dropdown size="lg">
+                            <Dropdown.Toggle 
+                                variant="primary" 
+                                id="dropdown-basic" 
+                            >
+                                Select a location
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1" onClick={handleLocationChange}>East</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3" onClick={handleLocationChange}>North</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2" onClick={handleLocationChange}>North-East</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2" onClick={handleLocationChange}>South</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleLocationChange}>Central</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <hr/>
-                <h2 className='mt-3'>Eat How Much? <span style={{ color: 'green' }}>{selectedBudget}</span></h2>
-                <Dropdown size="lg">
-                    <Dropdown.Toggle 
-                        variant="success" 
-                        id="dropdown-basic" 
-                    >
-                        Select a budget
-                    </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1" onClick={handleLocationChange}>East</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3" onClick={handleLocationChange}>North</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2" onClick={handleLocationChange}>North-East</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2" onClick={handleLocationChange}>South</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleLocationChange}>Central</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='col-6 ps-0'>
+                        <h2>Eat How Much? <span style={{ color: 'green' }}>{selectedBudget}</span></h2>
+                        <Dropdown size="lg">
+                            <Dropdown.Toggle 
+                                variant="success" 
+                                id="dropdown-basic" 
+                            >
+                                Select a budget
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1" onClick={handleBudgetChange}>Fancy 💸💸</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3" onClick={handleBudgetChange}>Gai Gai 💰💰</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2" onClick={handleBudgetChange}>Budget ✅✅</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleBudgetChange}>Grass 🌱🌱</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1" onClick={handleBudgetChange}>Fancy 💸💸</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3" onClick={handleBudgetChange}>Gai Gai 💰💰</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2" onClick={handleBudgetChange}>Budget ✅✅</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleBudgetChange}>Grass 🌱🌱</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </div>
                 <hr/>
-                <h2 className='mt-3'>Eat What? <span style={{ color: 'red' }}>{selectedCuisine}</span></h2>
-                <Dropdown size="lg">
-                    <Dropdown.Toggle 
-                        variant="danger" 
-                        id="dropdown-basic" 
-                    >
-                        Select a cuisine
-                    </Dropdown.Toggle>
+                <div className='row'>
+                    <div className='col-6 pe-0'>
+                        <h2>Eat What? <span style={{ color: 'red' }}>{selectedCuisine}</span></h2>
+                        <Dropdown size="lg">
+                            <Dropdown.Toggle 
+                                variant="danger" 
+                                id="dropdown-basic" 
+                            >
+                                Select a cuisine
+                            </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item href="#/action-1" onClick={handleCuisineChange}>Japanese 🇯🇵</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3" onClick={handleCuisineChange}>Western 🍽️</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2" onClick={handleCuisineChange}>Cafe ☕</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Korean 🇰🇷</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Chinese 🧧</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Asian 👲🐘🇲🇾🇻🇳</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Fast-Food 🍔</Dropdown.Item>
-                        <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Dessert 🍧</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <hr/>
-                <h2 className='mt-3'>Eat Before?</h2>
-                <ButtonGroup>
-                    <Button variant={isEatBefore ? 'success' : 'secondary'} size="md" onClick={handleLeftClick}>Yes</Button>
-                    <Button variant={!isEatBefore ? 'danger' : 'secondary'} size="md" onClick={handleRightClick}>No</Button>
-                </ButtonGroup>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1" onClick={handleCuisineChange}>Japanese 🇯🇵</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3" onClick={handleCuisineChange}>Western 🍽️</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2" onClick={handleCuisineChange}>Cafe ☕</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Korean 🇰🇷</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Chinese 🧧</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Asian 👲🐘🇲🇾🇻🇳</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Fast-Food 🍔</Dropdown.Item>
+                                <Dropdown.Item href="#/action-4" onClick={handleCuisineChange}>Dessert 🍧</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='col-6 ps-0'>
+                        <h2>Eat Before?</h2>
+                        <ButtonGroup>
+                            <Button variant={isEatBefore ? 'success' : 'secondary'} size="md" onClick={handleLeftClick}>Yes</Button>
+                            <Button variant={!isEatBefore ? 'danger' : 'secondary'} size="md" onClick={handleRightClick}>No</Button>
+                        </ButtonGroup>
+                    </div>
+                </div>
                 <hr/>
                 <div>
                     <Button variant="primary" size="lg" className='my-2 mb-4' onClick={handleSubmit}>Submit</Button>
